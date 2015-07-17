10d52c7ce69af5d41dcfdc38295326872155c1e1
Js41637 ftw <js41637@gmail.com>
2015-07-18 00:22:49 +1000

    Misc CSS changes and design improvements

--- Slightly-Better-BattleLog/Slightly Better BattleLog.user.js
+++ Slightly-Better-BattleLog/Slightly Better BattleLog.user.js
@@ -25,15 +25,14 @@ var initialize = function() {
     $J = jQuery;
 
     // Minified CSS to add into the page
-    $J("<style type='text/css'>#toggle-sidepanel{position:fixed;right:213px;transform:rotate(90deg);top:37px;background-color:rgba(0,0,0,.6);padding:10px;z-index:5000;font-size:15px;text-transform:uppercase;transition:background-color,right .2s ease-in-out;cursor:pointer}#toggle-sidepanel:hover{background-color:rgba(60,60,60,.5)}#comcenter-friends{transition:width .2s ease-in-out}#base-header,#unified-game-manager{transition:right .2s ease-in-out}#viewport{transition:padding .2s ease-in-out}.hidesidepanel #comcenter-friends{width:0!important}.hidesidepanel #base-header,.hidesidepanel #unified-game-manager{right:0!important}.hidesidepanel #viewport{padding-right:0!important}.hidesidepanel #toggle-sidepanel{right:-24px !important}#base-container:before{height:64px;}.game-bar .battlelog-logo{position:absolute;top:0px;left:30px;z-index:1000}.game-bar .battlelog-logo .logo{display:block;width:70px;height:48px;background:url(//d34ymitoc1pg7m.cloudfront.net/common/battlelog-logo-082bd9ee.png) 0 50% no-repeat;background-size:contain;}#uioverlay .uioverlaysectionleft{left:20px !important;}</style>").appendTo("head");
+    $J("<style type='text/css'>#toggle-sidepanel{position:fixed;right:-24px;transform:rotate(90deg);top:37px;background-color:rgba(0,0,0,.6);padding:10px;z-index:5000;font-size:15px;text-transform:uppercase;transition:background-color,right .2s ease-in-out;cursor:pointer}#toggle-sidepanel:hover{background-color:rgba(60,60,60,.5)}#comcenter-friends{width:0;transition:width .2s ease-in-out}#base-bf4-html #base-header,#base-bf4-html #unified-game-manager{right:0;transition:right .2s ease-in-out}#base-bf4-html #viewport{padding-right:0;transition:padding .2s ease-in-out}.show-sidepanel #comcenter-friends{width:237px!important}.show-sidepanel #base-header,.show-sidepanel #unified-game-manager{right:237px!important}.show-sidepanel #viewport{padding-right:237px!important}.show-sidepanel #toggle-sidepanel{right:213px!important}#base-container:before{height:64px}.game-bar .battlelog-logo{position:absolute;top:0;left:30px;z-index:1000}.game-bar .battlelog-logo .logo{display:block;width:70px;height:48px;background:url(//d34ymitoc1pg7m.cloudfront.net/common/battlelog-logo-082bd9ee.png) 0 50% no-repeat;background-size:contain}#uioverlay .uioverlaysectionleft{left:20px!important}#serverbrowser-show .bblog-local-comment{display:none}#selected-server-scoreboard .box-content{min-height:67px}#comcenter-tab-friends-content{width:237px!important}</style>").appendTo("head");
 
     // Delay to wait for BattleLogs weird DOM
-    setTimeout(function() { waitForBattleLog(); }, 550);
+    setTimeout(function() { waitForBattleLog(); }, 600);
 }
 
 var waitForBattleLog = function() {
     console.info("BattleLog loaded, hopefully")
-    hideSidePanel();
     $J('body').append('<div id="toggle-sidepanel">Toggle</div>');
 
     $J('#toggle-sidepanel').on('click', function() {
@@ -55,12 +54,12 @@ var waitForBattleLog = function() {
 
 var hideSidePanel = function() {
     status = 1;
-    $J('body').addClass('hidesidepanel');
+    $J('body').removeClass('show-sidepanel');
 };
 
 var showSidePanel = function() {
     status = 0;
-    $J('body').removeClass('hidesidepanel');
+    $J('body').addClass('show-sidepanel');
 };
 
 checkjQuery();
