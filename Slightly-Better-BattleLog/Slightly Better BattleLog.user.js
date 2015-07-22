056c820dd1e2619cd9bda83f296c4417429fabed
Js41637 ftw <js41637@gmail.com>
2015-07-23 00:25:22 +1000

    Fix Stupid BLog error and smarter leaving warning
    Fixed an error caused by the BattleScreen loop that is trying to update
    an element that no longer exists.
    Made the 'your leaving the page while your in a queue' warning to be
    smarter. It will only show if you are in a queue where leaving would
    cancel it but wont show if you're already in a game where it can be
    safely closed.

--- Slightly-Better-BattleLog/Slightly Better BattleLog.user.js
+++ Slightly-Better-BattleLog/Slightly Better BattleLog.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Slightly Better Battlelog
-// @version     1.0
+// @version     1.1
 // @author      Js41637
 // @match       http://battlelog.battlefield.com/bf4/*
 // @grant       none
@@ -25,7 +25,7 @@ var initialize = function() {
     $J = jQuery;
 
     // Minified CSS to add into the page
-    $J("<style type='text/css'>#toggle-sidepanel{position:fixed;right:-24px;transform:rotate(90deg);top:37px;background-color:rgba(0,0,0,.6);padding:10px;z-index:5000;font-size:15px;text-transform:uppercase;transition:background-color,right .2s ease-in-out;cursor:pointer}#toggle-sidepanel:hover{background-color:rgba(60,60,60,.5)}#comcenter-friends{width:0;transition:width .2s ease-in-out}#base-bf4-html #base-header,#base-bf4-html #unified-game-manager{right:0;transition:right .2s ease-in-out}#base-bf4-html #viewport{padding-right:0;transition:padding .2s ease-in-out}.show-sidepanel #comcenter-friends{width:237px!important}.show-sidepanel #base-header,.show-sidepanel #unified-game-manager{right:237px!important}.show-sidepanel #viewport{padding-right:237px!important}.show-sidepanel #toggle-sidepanel{right:213px!important}#base-container:before{height:64px}.game-bar .battlelog-logo{position:absolute;top:0;left:30px;z-index:1000}.game-bar .battlelog-logo .logo{display:block;width:70px;height:48px;background:url(//d34ymitoc1pg7m.cloudfront.net/common/battlelog-logo-082bd9ee.png) 0 50% no-repeat;background-size:contain}#uioverlay .uioverlaysectionleft{left:20px!important}#serverbrowser-show .bblog-local-comment{display:none}#selected-server-scoreboard .box-content{min-height:67px}#comcenter-tab-friends-content{width:237px!important}#bblog-icon{top:58px!important}#bblog-teamspeak{top:217px!important}</style>").appendTo("head");
+    $J("<style type='text/css'>#toggle-sidepanel{position:fixed;right:-24px;transform:rotate(90deg);top:37px;background-color:rgba(0,0,0,.6);padding:10px;z-index:5000;font-size:15px;text-transform:uppercase;transition:background-color,right .2s ease-in-out;cursor:pointer}#toggle-sidepanel:hover{background-color:rgba(60,60,60,.5)}#comcenter-friends{width:0;transition:width .2s ease-in-out}#base-bf4-html #base-header,#base-bf4-html #unified-game-manager{right:0;transition:right .2s ease-in-out}#base-bf4-html #viewport{padding-right:0;transition:padding .2s ease-in-out}.show-sidepanel #comcenter-friends{width:237px!important}.show-sidepanel #base-header,.show-sidepanel #unified-game-manager{right:237px!important}.show-sidepanel #viewport{padding-right:237px!important}.show-sidepanel #toggle-sidepanel{right:213px!important}#base-container:before{height:64px}.game-bar .battlelog-logo{position:absolute;top:0;left:30px;z-index:1000}.game-bar .battlelog-logo .logo{display:block;width:70px;height:48px;background:url(//d34ymitoc1pg7m.cloudfront.net/common/battlelog-logo-082bd9ee.png) 0 50% no-repeat;background-size:contain}#uioverlay .uioverlaysectionleft{left:20px!important}#serverbrowser-show .bblog-local-comment{display:none}#selected-server-scoreboard .box-content{min-height:67px}#comcenter-tab-friends-content{width:237px!important}#serverbrowser .servers-list tbody .server-row{height:63px}#bblog-icon{top:58px!important}#bblog-teamspeak{top:217px!important}#serverbrowser .servers-list tbody .server-row{height:63px}</style>").appendTo("head");
 
     // Delay to wait for BattleLogs weird DOM
     setTimeout(function() { waitForBattleLog(); }, 600);
@@ -50,6 +50,18 @@ var waitForBattleLog = function() {
     // Moves the battlelog logo to secondary header and then removes top header
     $J('#community-bar .battlelog-logo').appendTo($J('#base-header .game-bar'));
     $J('#community-bar').empty();
+
+
+    // Fix stupid BattleLog that doesn't check if variables exist before it tries to update
+    // Which needs another timeout cause BattleLog :/
+    setTimeout(function() {
+        Surface.Renderer.updateElement = function(replaceEl, html) {
+            if(replaceEl && html) {
+                replaceEl.innerHTML = Surface.Renderer.createElement(html).innerHTML;
+            }
+        }
+    }, 1000);
+    
 };
 
 var hideSidePanel = function() {
@@ -65,5 +77,7 @@ var showSidePanel = function() {
 checkjQuery();
 
 window.onbeforeunload = function() {
-    return "Are you sure you want to leave? If you're in a queue your spot will be lost!";
+    if (unifiedgamemanager.getUgmPresence().hidden == false && unifiedgamemanager.getUgmPresence().isPlaying == false) {
+        return "You are currently in a queue, leaving page or closing browser will loose your position!";
+    }
 };
