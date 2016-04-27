4759755434bcb7c991e3b27d8690590ccf47082e
Js41637 ftw <js41637@gmail.com>
2016-04-27 19:01:09 +1000

    Fuck that off

--- Slighty Better Google Photos/Slighty Better Google Photos.user.js
+++ Slighty Better Google Photos/Slighty Better Google Photos.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Slighty Better Google Photos
-// @version     1.0
+// @version     1.1
 // @author      Js41637
 // @match       *://photos.google.com/albums
 // @grant       none
@@ -28,108 +28,95 @@ var selectors = {
   albumPad: '.D14YYd'
 };
 
-// Select all the albums
+// Select all the albums and sort em into groups
 function getAlbumsOnPage() {
-  return new Promise(function(resolve) {
-    document.querySelectorAll(selectors.albums).forEach(function(album) {
-      var out = {
-        name: album.querySelector('.FmgwTd').innerText,
-        html: album
-      };
-      if (out.name.indexOf('Wallpapers') === 0) {
-        out.name.indexOf('Games') > 0 ? Albums['Game Wallpapers'].push(out) : Albums.Wallpapers.push(out);
-      } else {
-        Albums.Misc.push(out);
-      }
-    });
-    padding = document.querySelectorAll(selectors.albumPad);
-    return resolve(true);
+  document.querySelectorAll(selectors.albums).forEach(function(album) {
+    var out = {
+      name: album.querySelector('.FmgwTd').innerText,
+      html: album
+    };
+    if (out.name.indexOf('Wallpapers') === 0) {
+      out.name.indexOf('Games') > 0 ? Albums['Game Wallpapers'].push(out) : Albums.Wallpapers.push(out);
+    } else {
+      Albums.Misc.push(out);
+    }
   });
 }
 
 // Sort array by name alphabetically
 // Feel free to cringe
 function sortAlbumsByName() {
-  return new Promise(function(resolve) {
-    Albums.Wallpapers.sort(function(a, b) {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
-    });
-    Albums['Game Wallpapers'].sort(function(a, b) {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
-    });
-    Albums.Misc.sort(function(a, b) {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
-    });
-    return resolve(true);
+  Albums.Wallpapers.sort(function(a, b) {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
+  Albums['Game Wallpapers'].sort(function(a, b) {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
+  Albums.Misc.sort(function(a, b) {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
   });
 }
 
 // To prevent the flexbox showing 1 massive fucking image
 function addPaddingToAlbums() {
-  return new Promise(function(resolve) {
-    for (var Album in Albums) {
-      for (var i = 0; i < 13; i++) {
-        // Apparently you need to create a new element every time, adding the same element gets ignored
-        var pad = document.createElement('div');
-        pad.className = selectors.albumPad.slice(1);
-        var out = {
-          name: 'pad',
-          html: pad
-        };
-        Albums[Album].push(out);
-      }
+  for (var Album in Albums) {
+    for (var i = 0; i < 13; i++) {
+      // Apparently you need to create a new element every time, adding the same element gets ignored
+      var pad = document.createElement('div');
+      pad.className = selectors.albumPad.slice(1);
+      var out = {
+        name: 'pad',
+        html: pad
+      };
+      Albums[Album].push(out);
     }
-    return resolve(true);
-  });
+  }
 }
 
 // Umm, create new group for albums and yeah
 // #nojQueryLyfe
 function createAlbumGroups() {
-  return new Promise(function(resolve) {
+  // Set shit up
+  var rootContainer = document.querySelector(selectors.rootContainer),
+    albumsContainer = document.querySelector(selectors.albumsContainer);
+
+  // Blank Slate
+  albumsContainer.remove();
+
+  // Setup new albums
+  for (var Album in Albums) {
     // Set shit up
-    var rootContainer = document.querySelector(selectors.rootContainer),
-      albumsContainer = document.querySelector(selectors.albumsContainer);
-
-    // Blank Slate
-    albumsContainer.remove();
-
-    // Setup new albums
-    for (var Album in Albums) {
-      // Set shit up
-      var newAlbum = document.createElement('div'),
-        newHeader = document.createElement('h2');
-      newAlbum.id = Album.split(' ').join('');
-      newAlbum.className = selectors.albumsContainer.slice(1);
-      newHeader.innerText = Album;
-      newHeader.style.width = '100%';
-      newAlbum.appendChild(newHeader);
-      rootContainer.appendChild(newAlbum);
-    }
-    return resolve(true);
-  });
+    var newAlbum = document.createElement('div'),
+      newHeader = document.createElement('h2');
+    // Get shit just right
+    newAlbum.id = Album.split(' ').join('');
+    newAlbum.className = selectors.albumsContainer.slice(1);
+    newHeader.innerText = Album;
+    newHeader.style.width = '100%';
+    newAlbum.appendChild(newHeader);
+    rootContainer.appendChild(newAlbum);
+  }
 }
 
 // Starts of the magic and then adds the albums to their respective categories
 function updateAlbumsOnPage() {
-  getAlbumsOnPage()
-    .then(sortAlbumsByName())
-    .then(addPaddingToAlbums())
-    .then(createAlbumGroups())
-    .then(function() {
-      for (var Album in Albums) {
-        var curAlbum = document.querySelector('#' + Album.split(' ').join(''));
-        Albums[Album].forEach(function(album) {
-          curAlbum.appendChild(album.html);
-        });
-      }
+  getAlbumsOnPage();
+  sortAlbumsByName();
+  addPaddingToAlbums();
+  createAlbumGroups();
+
+  for (var Album in Albums) {
+    var curAlbum = document.querySelector('#' + Album.split(' ').join(''));
+    Albums[Album].forEach(function(album) {
+      curAlbum.appendChild(album.html);
     });
+  }
 }
 
 // 1 second delay before messing with the DOM
