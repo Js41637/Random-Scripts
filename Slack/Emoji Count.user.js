3e15f2380dbf3829e9b46587e3cc8627777b97bc
Js41637 ftw <js41637@gmail.com>
2016-06-18 16:04:40 +1000

    Increase sexyness by a factor of approximately 1 million
    tl;dr shows how many emojis each user has added

--- Slack/Emoji Count.user.js
+++ Slack/Emoji Count.user.js
@@ -1,10 +1,46 @@
 // ==UserScript==
 // @name         Emoji Count
-// @version      0.1
+// @version      0.2
 // @description  Displays how many custom emojis you got
 // @author       Js41637
 // @match        https://*.slack.com/customize/emoji
 // ==/UserScript==
 
-var length = $('#custom_emoji tbody > .emoji_row').length;
-$('#custom_emoji').prepend('<h5>You currently have ' + length + ' custom Emoji</h5>');
+var emojis = document.querySelectorAll('#custom_emoji tbody > .emoji_row');
+var users = {};
+var expanded = false;
+
+emojis.forEach(function(emoji) {
+  var adder = emoji.querySelector('.author_cell').innerText.trim();
+  if (adder in users) {
+    users[adder]++;
+  } else {
+    users[adder] = 1;
+  }
+});
+
+var text = '<h5 id="ec_count">You currently have ' + emojis.length + ' custom Emoji added by ' + Object.keys(users).length + ' people</h5>';
+var detailsText = ' <a id="ec_viewDetails">View Details</a>';
+$('#custom_emoji').prepend(text);
+$('#ec_count').append(detailsText);
+
+var sorted = Object.keys(users).sort(function(a, b) {
+  return users[b] - users[a];
+});
+
+var list = sorted.map(function(user) {
+  return user + ': ' + users[user];
+});
+var listTemplate = '<div id="ec_list" style="display: none">' + list.join('<br>') + '</div>';
+$('#ec_count').append(listTemplate);
+$('#ec_viewDetails').click(function() {
+  if (!expanded) {
+    expanded = true;
+    $('#ec_list').slideDown();
+    $('#ec_viewDetails').text('Hide Details');
+  } else {
+    expanded = false;
+    $('#ec_list').slideUp();
+    $('#ec_viewDetails').text('View Details');
+  }
+});
