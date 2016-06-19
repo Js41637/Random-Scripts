cd59ec56a0e654a5e0b4274182939f8b4552425e
Js41637 ftw <js41637@gmail.com>
2016-06-19 19:27:02 +1000

    Add Delete All on page button

--- Slack/Delete Button on Files List.user.js
+++ Slack/Delete Button on Files List.user.js
@@ -1,17 +1,20 @@
 // ==UserScript==
 // @name         Delete Slack File
-// @version      0.1
+// @version      0.2
 // @description  Adds a delete button to the file list
 // @author       Js41637
 // @match        https://*.slack.com/files*
 // @exclude      https://*.slack.com/files/*/*
 // ==/UserScript==
 
+var deleteAllButton = '<a class="df_deleteAll btn btn_danger small_right_margin small_bottom_margin"><i class="ts_icon ts_icon_trash small_right_margin"></i>Delete all on page</a>';
 var deleteButton = '<button class="btn_icon btn btn_outline ts_tip_btn ts_tip ts_tip_top df_deleteFile">';
 var deleteIcon = '<span class="ts_icon ts_icon_trash" style="border:0;color:#555459"></span><div class="star_message ts_tip_tip">Delete</div>';
 var deleteFileButton = deleteButton + deleteIcon + '</button>';
 
 var files;
+var fileIDs = [];
+var errors = 0;
 
 function waitForDOM(attempt) {
   if (!$('#files_list .file_item').length && attempt < 7) {
@@ -22,6 +25,7 @@ function waitForDOM(attempt) {
     console.debug("Found files");
     files = $('#files_list .file_item');
     addDeleteButtonToFiles();
+    addDeleteAllButton();
   } else {
     console.error("Couldn't find any files");
   }
@@ -31,6 +35,7 @@ function addDeleteButtonToFiles() {
   files.each(function(i, file) {
     var fileID = file.getAttribute('data-file-id');
     if (fileID) {
+      fileIDs.push(fileID);
       $(deleteFileButton).attr('data-file-id', fileID).insertBefore($(file).find('.file_star'));
     } else {
       return console.error("Found no file ID for file", i);
@@ -46,19 +51,10 @@ function bindClicks() {
     e.preventDefault();
 
     var fileID = e.currentTarget.getAttribute('data-file-id');
-    var apiKey = TS.boot_data.api_token;
-    if (fileID && apiKey) {
-      $.post('https://slack.com/api/files.delete', {
-        token: apiKey,
-        file: fileID
-      }, function(resp) {
-        if (resp && resp.ok) {
-          console.debug("Successfully deleted file", fileID);
-          $('#file_' + fileID).css('opacity', 0.3);
-        }
-      });
+    if (fileID) {
+      deleteFile(fileID);
     } else {
-      console.error("WTF? No fileID or APIKey?");
+      console.error("WTF? No fileID?");
     }
   });
 }
@@ -69,6 +65,68 @@ function hoverButtons() {
     $(e.currentTarget).find('span').css('color', '#eb4d5c');
   }, function(e) {
     $(e.currentTarget).find('span').css('color', '#555459');
+  });
+}
+
+function addDeleteAllButton() {
+  var delAll = $(deleteAllButton).insertBefore($('.col.span_1_of_3'));
+  delAll.click(function() {
+    TS.generic_dialog.start({
+      title: "Delete files",
+      body: "Are you sure you want to permanently delete all files on this page? Dis can not be undone!",
+      show_cancel_button: true,
+      show_go_button: true,
+      go_button_class: "btn_danger",
+      go_button_text: "Yes, delete all files",
+      cancel_button_text: "Cancel",
+      onGo: function() {
+        deleteAll(delAll);
+      }
+    });
+  });
+}
+
+function deleteAll(btn) {
+  var offset = 0;
+  var dun = fileIDs.length;
+  btn.css('pointer-events', 'none');
+  fileIDs.forEach(function(file) {
+    setTimeout(function() {
+      console.debug("Deleting file", file, dun);
+      deleteFile(file);
+      dun--;
+      btn.text("Deleted " + (fileIDs.length - dun) + '/' + fileIDs.length);
+      if (dun === 0) {
+        setTimeout(function() {
+          if (errors) {
+            btn.text("Finished with " + errors + " errors");
+          } else {
+            btn.text("Finished");
+          }
+        }, 450);
+      }
+    }, 210 + offset);
+    offset += 210;
+  });
+}
+
+var apiKey = TS.boot_data.api_token;
+
+function deleteFile(id) {
+  if (!apiKey) {
+    return console.error("WTF? No api key?");
+  }
+  $.post('https://slack.com/api/files.delete', {
+    token: apiKey,
+    file: id
+  }, function(resp) {
+    if (resp && resp.ok) {
+      console.debug("Successfully deleted file", id);
+      $('#file_' + id).css('opacity', 0.3);
+    } else {
+      errors++;
+      console.error("Failed to delete file", resp.error);
+    }
   });
 }
 
