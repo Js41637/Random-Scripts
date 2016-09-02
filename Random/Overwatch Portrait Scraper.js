aefe4eebb5dc6acf89466085a00dec88397ca514
Js41637 ftw <js41637@gmail.com>
2016-09-02 19:53:59 +1000

    Sexify the fuck outta this

--- Random/Overwatch Portrait Scraper.js
+++ Random/Overwatch Portrait Scraper.js
@@ -1,25 +1,82 @@
-var numbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-var start = '0x0250000000000'
-var url = 'https://blzgdapipro-a.akamaihd.net/game/playerlevelrewards/'
-var index = 9
-var index2 = 0
+const url = 'https://blzgdapipro-a.akamaihd.net/game/playerlevelrewards/'
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 
-function doShit() {
-    numbers.forEach(function(num) {
-        console.log(start + index + index2 + num)
-        var cont = document.createElement('div')
-        var img = document.createElement('img')
-        var img2 = document.createElement('img')
-        img.src = url + start + index + index2 + num + '_Border.png'
-        img2.src = url + start + index + index2 + num + '_Rank.png'
-        var thing = document.body.appendChild(cont)
-        thing.appendChild(img)
-        thing.appendChild(img2)
-        thing.innerHTML = thing.innerHTML + (start + index + index2 + num)
+const start = '0x0250000000000'
+let index = 9
+let index2 = 0
+
+let container = document.createElement('div')
+container.id = "border_cont"
+container = document.body.appendChild(container)
+
+var doShit = () => {
+  numbers.forEach((num) => {
+    let currentNumber = start + index + index2 + num
+    let borderURL = url + currentNumber + '_Border.png'
+    let rankURL = url + currentNumber + '_Rank.png'
+    makeRequest(borderURL).then(() => {
+      let cont = document.createElement('div')
+      let img = document.createElement('img')
+      let img2 = document.createElement('img')
+      img.src = borderURL
+      img2.src = rankURL
+
+      let thing = container.appendChild(cont)
+      thing.appendChild(img)
+      thing.appendChild(img2)
+      thing.innerHTML = thing.innerHTML + currentNumber
+
     })
-    index2++
+  })
+  index2++
 }
 
-for (var i = 0; i < 10; i++) {
+let offset = 0
+for(var i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.info("~~ STARTING", start + index + index2)
     doShit()
+  }, 2000 + offset);
+  offset += 2000
+
 }
+
+var makeRequest = what => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest()
+
+    xhr.addEventListener("readystatechange", function() {
+      if(this.readyState === 4) {
+        if(this.status === 403) return reject()
+        else if(this.status === 0) return resolve()
+        else {
+          console.info("Got unknown response", this.status)
+        }
+      }
+    })
+
+    xhr.open("GET", what)
+    xhr.send()
+  })
+}
+
+var css = `
+#border_cont {
+  display: flex;
+  flex-wrap: wrap;
+}
+#border_cont div {
+  text-align: center;
+  border-bottom: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  min-height: 300px;
+  justify-content: space-between;
+}
+#border_cont div img:nth-of-type(2) {
+  margin-top: -120px;
+}
+`
+var style = document.createElement('style')
+style.innerHTML = css
+document.head.appendChild(style)
